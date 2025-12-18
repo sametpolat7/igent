7f411ffef7729713a090eb1d3aeb98dd1bf44e2d
Samet Polat <samet.polat@iwallet.com.tr>
2025-12-18 18:48:51 +0300

    feat: Add deployment planning feature with UI integration

--- src/preload/index.cjs
+++ src/preload/index.cjs
@@ -1,5 +1,7 @@
-const { contextBridge } = require('electron');
+const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('igent', {
   version: () => '1.0.0',
+
+  planDeploy: (branch) => ipcRenderer.invoke('agent:plan-deploy', { branch }),
 });
